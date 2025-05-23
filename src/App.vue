883087f82ae89eba503091aafd5acf72740050d0
Amk0 <amko.licina14@gmail.com>
2025-05-23 12:14:39 +0200

    rade tabele

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Welcome to the project</h1>
+  <AuthLayout>
+    <RouterView v-slot="{ Component, route }">
+      <Suspense v-if="Component" :timeout="0">
+        <Component :is="Component" :key="route.name"></Component>
+
+        <template #fallback>
+          <span>Loading ..</span>
+        </template>
+      </Suspense>
+    </RouterView>
+  </AuthLayout>
 </template>
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>WELCOME -Home Page-</h1>
+   
+  </div>
+</template>
